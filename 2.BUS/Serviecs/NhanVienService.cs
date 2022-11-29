ae9c59de1ef1f4edd5fd74142823f5a2e1b5c9eb
ongan0 <103545126+ongan0@users.noreply.github.com>
2022-11-29 13:20:02 +0700

    Tung
    chức năng đăng nhập
    chức năng quyên mật khẩu

---
+++ 2.BUS/Serviecs/NhanVienService.cs
@@ -0,0 +1,22 @@
+﻿using _1.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.BUS.Serviecs
+{
+    public class NhanVienService
+    {
+        NhanVienRepository nhanVienRepository = new NhanVienRepository();
+        public int checkTaiKhoan(string gmail, string matkhau)
+        {
+           return nhanVienRepository.checkTaiKhoan(gmail,matkhau);
+        }
+        public bool checkTT(string gmail, string sdt,string matkhau)
+        {
+            return nhanVienRepository.checkTT(gmail, sdt,matkhau);
+        }
+    }
+}
